65822bb54e0986658f5dea74e8b9adc032c12103
prasanthoyasco <prasanthoyasco@gmai.com>
2025-06-11 17:52:28 +0530

    Category product list added

--- src/Components/CategoriesPage/CategoriesHeader/CategoriesHeader.jsx
+++ src/Components/CategoriesPage/CategoriesHeader/CategoriesHeader.jsx
@@ -3,9 +3,12 @@ import './CategoriesHeader.css'
 import Image from '../../../Assets/CategoriesImge/Knob Shop/image 36.jpg'
 import CategoriesGrid from '../CategoriesGrid/CategoriesGrid'
 import CategoriesBanner from '../CategoriesBanner/CategoriesBanner'
+import NavbarTop from '../../Navbar/NavbarTop/NavbarTop'
+import Footer from '../../Footer/Footer'
 function CategoriesHeader() {
   return (
     <>
+    <NavbarTop/>
     <div className='categories-page-container'>
       <img src={Image} className='background-image'/>
       <div className='categories-image-overlay'></div>
@@ -16,6 +19,7 @@ function CategoriesHeader() {
     </div>
     <CategoriesGrid/>
     <CategoriesBanner/>
+    <Footer/>
     </>
   )
 }
