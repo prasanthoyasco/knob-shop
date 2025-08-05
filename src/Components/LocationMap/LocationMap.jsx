076177dbb864cee72d087fd1b45730de03584712
prasanthoyasco <prasanthoyasco@gmai.com>
2025-08-05 09:09:47 +0530

    4 loactions map have been added

--- src/Components/LocationMap/LocationMap.jsx
+++ src/Components/LocationMap/LocationMap.jsx
@@ -7,7 +7,7 @@ const LocationMap = () => {
       <div className="map-container">
         <iframe
           title="Knobsshop Google Map"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62654.5700716275!2d77.00853840059774!3d11.045327157984481!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba857acf26cf9e9%3A0x867b3ac4ec9ea8ac!2sKnobs%20Shop%20Goldwins!5e0!3m2!1sen!2sin!4v1754131637083!5m2!1sen!2sin"
+          src="https://www.google.com/maps/d/embed?mid=1DWLaSKN-jdvsPYApqmsFjX2JnAbJ_wM&noprof=1"
           width="100%"
           height="450"
           allowFullScreen=""
