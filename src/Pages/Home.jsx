be9510714e83576c3190a1518823e4699da70e0a
JagadishRamakrishnan <jeevasumathi1357@gmail.com>
2025-08-07 11:17:20 +0530

    updated cart page bug

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -58,7 +58,7 @@ import cat2 from '../Assets/New folder/New folder/02.png'
 import cat3 from '../Assets/New folder/New folder/3.png'
 import cat7 from '../Assets/New folder/New folder/7.png'
 import cat8 from '../Assets/CategoriesImge/Knob Shop/doorClosser.png'
-import cat10 from '../Assets/CategoriesImge/Knob Shop/hexa.jpg'
+import cat10 from '../Assets/CategoriesImge/Knob Shop/Hexa bolt.png'
 import cat9 from '../Assets/CategoriesImge/Knob Shop/black rim.png'
 export const Home = () => {
   const [categories, setCategories] = useState([]);
